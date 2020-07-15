1657746dcd6a749cac34f862aefb7f0a711c3727
Dhruv Patel <dpatel@gasbuddy.com>
2020-07-15 15:27:07 -0600

    Remove Text component, add Flex component

---
+++ components/Flex.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+const Flex = ({ style: customStyle, ...props }) => (
+  <View
+    style={{ ...styles.flex, ...customStyle }}
+    {...props}
+  />
+);
+
+const FlexContent = ({ style: customStyle, width, ...props }) => (
+  <View
+    style={{ customStyle, width }}
+    {...props}
+  />
+)
+
+Flex.Content = FlexContent;
+
+export default Flex;
