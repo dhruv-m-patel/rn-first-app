3905fadd83d1a4fa51967ae20b3c39a30dd756f3
Dhruv Patel <dpatel@gasbuddy.com>
2020-07-28 11:08:52 -0600

    Add bottom tab navigation

--- src/MealsApp/components/Navigation.js
+++ src/MealsApp/components/Navigation.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import MealsScreen from '../screens/MealsScreen';
@@ -8,23 +11,59 @@ import FiltersScreen from '../screens/FiltersScreen';
 import RecipeScreen from '../screens/RecipeScreen';
 import theme from '../../common/theme';
 
-const config = {
+const screenNavigation = {
   Categories: CategoriesScreen,
-  Favorites: FavoritesScreen,
   Meals: MealsScreen,
   Filters: FiltersScreen,
   Recipe: RecipeScreen
 };
 
-const defaultStyles = {
+const screenNavigationOptions = {
   defaultNavigationOptions: {
     headerStyle: {
       backgroundColor: Platform.OS === 'android' ? theme.color.primary : undefined,
     },
     backgroundTintColor: Platform.OS === 'android' ? theme.baseTextColor : undefined,
   }
-}
+};
 
-const navigator = createStackNavigator(config, defaultStyles);
+const mainNavigator = createStackNavigator(screenNavigation, screenNavigationOptions);
 
-export default createAppContainer(navigator);
+const tabNavigation = {
+  Meals: {
+    screen: mainNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Menu',
+      tabBarIcon: tabInfo => (
+        <Ionicons
+          name="ios-restaurant"
+          size={25}
+          color={tabInfo.tintColor}
+        />
+      ),
+    },
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarLabel: 'My Favorites',
+      tabBarIcon: tabInfo => (
+        <Ionicons
+          name="ios-star"
+          size={25}
+          color={tabInfo.tintColor}
+        />
+      ),
+    },
+  },
+};
+
+const tabNavigationOptions = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'android' ? theme.color.accent : theme.baseTextColor,
+  },
+};
+
+const consolidatedNavigation = createBottomTabNavigator(tabNavigation, tabNavigationOptions);
+
+export default createAppContainer(consolidatedNavigation);
--- src/MealsApp/screens/FavoritesScreen.js
+++ src/MealsApp/screens/FavoritesScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import Screen from '../components/Screen';
 
 const FavoritesScreen = () => (
-  <Screen />
+  <Screen>
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  </Screen>
 );
 
 export default FavoritesScreen;
